b7751cd16d6194caca8c615abf7757712a157f1c
Thomas Payer <payer@raum42.at>
2016-10-13 22:32:29 +0200

    new asset system

--- config/assets.config.php
+++ config/assets.config.php
@@ -8,9 +8,8 @@ return [
     'assets' => [
         'directories' => [
             'media42' => [
-                'target' => 'public/assets/admin/media42',
+                'target' => 'admin/media42',
                 'source' => 'vendor/fruit42/media42/assets/dist/',
-                'base_url'  => '/assets/admin/media42',
             ],
         ],
     ],
