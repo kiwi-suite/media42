2d765763fa0112d2ad66bfec1f376e0a2539f401
Thomas Payer <payer@raum42.at>
2016-12-03 19:24:34 +0100

    added german translation

---
+++ data/language/de-DE.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    'label.media'                   => 'Medien',
+    'title.media'                   => 'Medien',
+    'button.browse'                 => 'Durchsuchen',
+    'button.upload'                 => 'Hochladen',
+    'button.upload_all'             => 'Alle Hochladen',
+    'button.cancel_all'             => 'Alle Abbrechen',
+    'button.remove_all'             => 'Alle entfernen',
+    'button.add'                    => 'Hinzufügen',
+    'field.size'                    => 'Größe',
+    'field.progress'                => 'Fortschritt',
+    'label.filename'                => 'Dateinamen',
+
+    'media.modal.delete.title'      => 'Mediendatei löschen',
+    'media.modal.delete.content'    => 'Mediendatei wirklich löschen',
+
+    'toaster.media.delete.title.success'    => 'Mediendatei gelöscht',
+    'toaster.media.delete.message.success'  => 'Mediendatei wurde erfolgreich gelöscht',
+
+    'start.upload'                  => 'Hier klicken um eine Datei hochzuladen',
+    'title.upload'                  => 'Hochladen',
+    'media.info-dropzone'           => 'Datein in dieses Feld ziehen',
+    'media.maxfilesize'             => 'Maximale Dateigröße:',
+    'media.error.filesize'          => 'Eine der Datein überschreitet die maximale Dateigröße von ',
+    'media.warning.image-too-small' => 'Bild zu klein',
+
+    'media.category.all'            => 'Alla Kategorien',
+    'media.category.default'        => 'Default',
+
+    'label.typeSelection.all'       => 'Alla',
+    'label.typeSelection.images'    => 'Bilder',
+    'label.typeSelection.pdf'       => 'PDF',
+];
