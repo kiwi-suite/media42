59b2aa0e59d9cb591099b41b7478c835f35f3e16
Thomas Payer <payer@raum42.at>
2017-02-17 09:11:07 +0100

    regenerate images

--- config/cli/cli.config.php
+++ config/cli/cli.config.php
@@ -8,10 +8,13 @@ return [
     'cli' => [
         'media-regenerate' => [
             'group'                     => 'media',
-            'route'                     => 'media-regenerate',
+            'route'                     => 'media-regenerate [--media=]',
             'command-name'              => RegenerateImagesCommand::class,
-            'description'               => 'Regenerate all images on given media dimension config',
-            'short_description'         => 'Regenerate all images',
+            'description'               => 'Regenerate all/one images on given media dimension config',
+            'short_description'         => 'Regenerate all/one images',
+            'options_descriptions'      => [
+                '--media'           => 'Given MediaId'
+            ]
         ],
     ],
 ];
